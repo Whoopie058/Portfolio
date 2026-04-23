38b88a9f58d7a77b5fb021bcc8e1123672a1bb2e
Whoopie058 <lauhan05@easv365.dk>
2026-04-23 23:51:19 +0200

    Rough Outline of Home Finished

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -40,21 +40,44 @@ onUnmounted(() => {
 
   <!-- Main content: alt indhold: lav i de forskellige /views/filer.vue -->
 
-  <!-- Windows container: border radius grey -->
-  <div class="border-grey-500 border-20 rounded-lg p-20 m-10">
+  <main class="px-4 pb-10 sm:px-6 lg:px-10">
+    <!-- Windows container: border radius grey -->
+    <div class="relative border-gray-500 border-20 rounded-lg p-20 bg-accent-500">
 
       <!-- top-windows-container: switched after clicking routes -->
-    <div class="top-windows-container">
-      <RouterView />
-    </div>
+      <div class="top-windows-container">
+        <RouterView />
+      </div>
 
-    <!-- bottom-menu-bar-windows: menu look-a-like from windows -->
-     <div class="bottom-menu-bar-windows">
-      <div class="menu-item">Windows ICON</div>
-      <div class="menu-item">Search field</div>
-      <div class="menu-item">Opened programs</div>
-     </div>
-  </div>
+      <!-- bottom-menu-bar-windows: menu look-a-like from windows -->
+      <div class="bottom-menu-bar-windows">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M6.133 21C4.955 21 4 20.02 4 18.81v-8.802c0-.665.295-1.295.8-1.71l5.867-4.818a2.09 2.09 0 0 1 2.666 0l5.866 4.818c.506.415.801 1.045.801 1.71v8.802c0 1.21-.955 2.19-2.133 2.19z"/><path d="M9.5 21v-5.5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2V21"/></g></svg>
+        <div class="menu-item">Search field</div>
+        <div class="menu-item">Opened programs</div>
+      </div>
+
+      <!-- Bottom Bar Windows (Open Apps) -->
+      <div class="absolute bottom-0 left-0 flex h-10 w-10 items-center justify-center border-2 border-black bg-white text-black">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" aria-hidden="true"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M6.133 21C4.955 21 4 20.02 4 18.81v-8.802c0-.665.295-1.295.8-1.71l5.867-4.818a2.09 2.09 0 0 1 2.666 0l5.866 4.818c.506.415.801 1.045.801 1.71v8.802c0 1.21-.955 2.19-2.133 2.19z"/><path d="M9.5 21v-5.5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2V21"/></g></svg>
+        
+      </div>
+
+      <div class="absolute bottom-0 left-10 flex h-10 w-40 items-center justify-center border-2 border-black bg-white text-black">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" aria-hidden="true"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M6.133 21C4.955 21 4 20.02 4 18.81v-8.802c0-.665.295-1.295.8-1.71l5.867-4.818a2.09 2.09 0 0 1 2.666 0l5.866 4.818c.506.415.801 1.045.801 1.71v8.802c0 1.21-.955 2.19-2.133 2.19z"/><path d="M9.5 21v-5.5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2V21"/></g></svg>
+        About Me
+      </div>
+
+      <div class="absolute bottom-0 left-50 flex h-10 w-40 items-center justify-center border-2 border-black bg-white text-black">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" aria-hidden="true"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M6.133 21C4.955 21 4 20.02 4 18.81v-8.802c0-.665.295-1.295.8-1.71l5.867-4.818a2.09 2.09 0 0 1 2.666 0l5.866 4.818c.506.415.801 1.045.801 1.71v8.802c0 1.21-.955 2.19-2.133 2.19z"/><path d="M9.5 21v-5.5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2V21"/></g></svg>
+        Skills
+      </div>
+
+      <div class="absolute bottom-0 left-90 flex h-10 w-40 items-center justify-center border-2 border-black bg-white text-black rounded-r-lg">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" aria-hidden="true"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M6.133 21C4.955 21 4 20.02 4 18.81v-8.802c0-.665.295-1.295.8-1.71l5.867-4.818a2.09 2.09 0 0 1 2.666 0l5.866 4.818c.506.415.801 1.045.801 1.71v8.802c0 1.21-.955 2.19-2.133 2.19z"/><path d="M9.5 21v-5.5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2V21"/></g></svg>
+        Contact
+      </div>
+    </div>
+  </main>
 
 </template>
 
